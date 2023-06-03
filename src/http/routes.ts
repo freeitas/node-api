254629fd56cc92a12826223e1357e7d69335e021
Guilherme Freitas <hfdsguilherme@gmail.com>
2023-06-03 10:03:32 -0300

    chore: controller

---
+++ src/http/routes.ts
@@ -0,0 +1,7 @@
+/* eslint-disable require-jsdoc */
+import {FastifyInstance} from 'fastify';
+import {register} from './controllers/register';
+
+export async function appRoutes(app: FastifyInstance) {
+  app.post('/users', register);
+}
