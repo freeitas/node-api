73789a8cdea6aa9297f66ab976c7a0beca8ce992
Guilherme Freitas <hfdsguilherme@gmail.com>
2023-06-03 13:49:24 -0300

    refactor: repository

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,5 +1,6 @@
 /* eslint-disable require-jsdoc */
 import {prisma} from '@/lib/prisma';
+import {PrismaUsersRepository} from '@/repositories/prisma-users-repository';
 import {hash} from 'bcryptjs';
 
 interface RegisterUseCaseRequest {
@@ -25,11 +26,11 @@ export async function registerUseCase({
     throw new Error('E-mail already exists.');
   }
 
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash,
-    },
+  const prismaUsersRepository = new PrismaUsersRepository();
+
+  await prismaUsersRepository.create({
+    name,
+    email,
+    password_hash,
   });
 }
